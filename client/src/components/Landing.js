b5c7a589b26596c9ae2f8cf7c10b5ebf671a0175
Xabin Rodriguez <xabin95@gmail.com>
2017-12-20 14:22:40 +0100

    Redeploying test

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Backfeed</h1>
+      Xe
+    </div>
+  );
+};
+
+export default Landing;
