92667c2c29363f663dc9bdad1b1431fcf4188af1
Xabin Rodriguez <xabin95@gmail.com>
2017-12-20 11:14:32 +0100

    Redux thunk and dispatching actions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./Header";
+const Dashboard = () => <h2>Dashboard - Placeholder</h2>;
+const SurveyNew = () => <h2>SurveyNew - Placeholder</h2>;
+const Landing = () => <h2>Landing - Placeholder</h2>;
 
 const App = () => {
-  return <div>Aupa!</div>;
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
